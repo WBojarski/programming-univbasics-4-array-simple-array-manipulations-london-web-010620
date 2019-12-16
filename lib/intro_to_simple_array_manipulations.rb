2f62f6d68c015c0085971904acb6991981b97e91
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-16 21:22:15 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array,string)
+	array.push(string)
+end
+
+def using_unshift(array,string)
+	array.using_unshift(string)
+end
+
+def using_pop(array)
+	array.pop
+end
+
+def pop_with_args(array, integer)
+	array.pop(number)
+end
+
+def using_shift(array)
+	array.shift
+end
+
+def shift_with_args(array, integer)
+	array.shift(number)
+end
+
+def using_concat(arr1,arr2)
+	arr1.using_concat(arr2)
+end
+
+def using_insert(array,element)
+	array.insert(3,element)
+end
+
+def using_uniq(array)
+	array.uniq
+end
+
+def using_flatten(array)
+	array.flatten
+end
+
+def using_delete(array,string)
+	array.delete(string)
+end
+
+def using_delete_at(array,integer)
+	array.delete[integer]
+end
